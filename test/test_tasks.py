b067c9fc28955dbfc2cb0d92edfaf5e15a1b8296
Joonalai <joona.laine@cofactor.fi>
2026-03-20 07:58:10 +0200

    Update copyright headers

--- test/test_tasks.py
+++ test/test_tasks.py
@@ -1,3 +1,23 @@
+# Copyright (C)
+# - 2019-2020 3Liz, info@3liz.org
+# - 2020-2025 Gispo OY, info@gispo.fi
+# - 2022-2026 qgis_plugin_tools contributors, info@osgeo.fi
+#
+# This file is part of qgis_plugin_tools.
+#
+# qgis_plugin_tools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# qgis_plugin_tools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with qgis_plugin_tools.  If not, see <https://www.gnu.org/licenses/>.
+
 import time
 from typing import Any
 
@@ -6,10 +26,6 @@ from qgis.core import Qgis
 from ..testing.utilities import SimpleTask, TestTaskRunner
 from ..tools.exceptions import QgsPluginException, TaskInterruptedException
 from ..tools.tasks import FunctionTask
-
-__copyright__ = "Copyright 2021, qgis_plugin_tools contributors"
-__license__ = "GPL version 3"
-__email__ = "info@gispo.fi"
 
 
 def fn(*args: Any, **kwargs: Any):  # noqa: ANN003
