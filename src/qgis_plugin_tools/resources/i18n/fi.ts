6703d24669fe8380296db0fee1fa670913608fd9
Joonalai <joona.laine@cofactor.fi>
2026-09-22 14:51:01 +0300

    chore: use qgis-plugin-copier-template
    Also drop support from python <12

---
+++ src/qgis_plugin_tools/resources/i18n/fi.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../../plugin.py" line="51"/>
+        <source>qgis_plugin_tools</source>
+        <translation type="unfinished">Unfinished</translation>
+    </message>
+</context>
+</TS>
